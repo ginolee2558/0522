6a1c7ba9daa04b53f6f807df54553657206ac412
ginolee2558 <leegino2558@gmail.com>
2026-05-22 11:27:22 +0800

    add_func reformatting

--- src/calculate.py
+++ src/calculate.py
@@ -1,7 +1,2 @@
-from calculate import add_func
-
-
-def test_add_func():
-    assert add_func(1, 2) == 3
-    assert add_func(0, 0) == 0
-    assert add_func(-1, -2) == -3
+def add_func(a, b):
+    return a + b
